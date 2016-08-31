ea90ae9519628d0c6723cf7b06446de85c0d8efb
XePeleato <edu@error404software.com>
2016-08-31 02:11:31 +0200

    Reupload from Huawei's website

---
+++ drivers/hisi/ap/platform/hi6210sft/soc_seceng_interface.h
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+#ifndef __SOC_SECENG_INTERFACE_H__
+#define __SOC_SECENG_INTERFACE_H__ 
+
+#ifdef __cplusplus
+    #if __cplusplus
+        extern "C" {
+    #endif
+#endif
+#ifdef __cplusplus
+    #if __cplusplus
+        }
+    #endif
+#endif
+#endif
