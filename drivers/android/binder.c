e5e088bf51e6f40f0004700396e8f2c98628264b
Arve Hjønnevåg <arve@android.com>
2017-05-27 20:23:19 +0200

    ANDROID: binder: Clear binder and cookiePrevents leaking pointers between processes
    BUG: 30768347
    Change-Id: Id898076926f658a1b8b27a3ccb848756b36de4ca
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -1787,7 +1787,9 @@ static void binder_transaction(struct binder_proc *proc,
 				fp->type = BINDER_TYPE_HANDLE;
 			else
 				fp->type = BINDER_TYPE_WEAK_HANDLE;
+			fp->binder = 0;
 			fp->handle = ref->desc;
+			fp->cookie = 0;
 			binder_inc_ref(ref, fp->type == BINDER_TYPE_HANDLE,
 				       &thread->todo);
 
@@ -1833,7 +1835,9 @@ static void binder_transaction(struct binder_proc *proc,
 					return_error = BR_FAILED_REPLY;
 					goto err_binder_get_ref_for_node_failed;
 				}
+				fp->binder = 0;
 				fp->handle = new_ref->desc;
+				fp->cookie = 0;
 				binder_inc_ref(new_ref, fp->type == BINDER_TYPE_HANDLE, NULL);
 				trace_binder_transaction_ref_to_ref(t, ref,
 								    new_ref);
@@ -1885,6 +1889,7 @@ static void binder_transaction(struct binder_proc *proc,
 			binder_debug(BINDER_DEBUG_TRANSACTION,
 				     "        fd %d -> %d\n", fp->handle, target_fd);
 			/* TODO: fput? */
+			fp->binder = 0;
 			fp->handle = target_fd;
 		} break;
 
